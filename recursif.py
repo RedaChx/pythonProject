3c595d4309ecdcdec503f24696b7335d02c670b5
Reda Chehabi <loxgaming99@gmail.com>
2023-11-24 01:28:12 +0100

    Initial commit

---
+++ recursif.py
@@ -0,0 +1,28 @@
+"""
+def fact(n):
+    if n == 1 :
+        return 1
+    else :
+        return fact(n - 1) * n
+a = int(input("Donner une valeur"))
+f = fact(a)
+print("le factoriel est", f)
+"""
+
+"""
+Variable : a , f : entier
+    debut fct
+    si n= 1
+        retourner 1
+    sinon
+        retourner fct(n-1)*n
+    finsi
+    fin fct
+debut
+ecrire(Donner un nombre)
+lire(a)
+f=fct(a)
+ecrire("le factoriel est",f)
+fin
+"""
+
